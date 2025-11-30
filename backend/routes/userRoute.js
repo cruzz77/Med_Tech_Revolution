b3cb4aa57ca31a582963b3f4449ecdac0881907d
Aditya Chopra <nytc120605@gmail.com>
2025-11-30 14:40:15 +0530

    Improved backend routes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { loginUser, registerUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment, paymentRazorpay, verifyRazorpay, paymentStripe, verifyStripe } from '../controllers/userController.js';
 import upload from '../middleware/multer.js';
 import authUser from '../middleware/authUser.js';
+
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser)
@@ -12,9 +13,5 @@ userRouter.post("/update-profile", upload.single('image'), authUser, updateProfi
 userRouter.post("/book-appointment", authUser, bookAppointment)
 userRouter.get("/appointments", authUser, listAppointment)
 userRouter.post("/cancel-appointment", authUser, cancelAppointment)
-userRouter.post("/payment-razorpay", authUser, paymentRazorpay)
-userRouter.post("/verifyRazorpay", authUser, verifyRazorpay)
-userRouter.post("/payment-stripe", authUser, paymentStripe)
-userRouter.post("/verifyStripe", authUser, verifyStripe)
 
 export default userRouter;
